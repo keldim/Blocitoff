76cba301fa68743c192b9faf30c4da81cd4d395c
Chris Yoo <woyoo@umich.edu>
2017-09-30 00:56:57 -0400

    controller added, show action and show view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  root 'welcome#index'
+  resources :users
+  get 'about' => 'welcome#about'
+  get 'home' => 'welcome#index'
+  root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
