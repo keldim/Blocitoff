cf1812d2b004ab8ae4d59c4404098f323bb55326
Chris Yoo <woyoo@umich.edu>
2017-10-02 02:45:16 -0400

    faker installed and used

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Item.create!(
+    name: Faker::Company.unique.name,
+    user: User.where(email: "woyoo@umich.edu").first
+  )
+end
+
+puts "Seed finished"
+puts "#{Item.count} items created"
